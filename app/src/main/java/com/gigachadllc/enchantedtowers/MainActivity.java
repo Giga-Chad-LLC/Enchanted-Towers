68be063e381732197415de4f662f5c080fa14277
dezd4r <naumkinvs03@gmail.com>
2023-02-24 22:28:27 +0100

    add google maps

--- app/src/main/java/com/gigachadllc/enchantedtowers/MainActivity.java
+++ app/src/main/java/com/gigachadllc/enchantedtowers/MainActivity.java
@@ -1,14 +1,26 @@
 package com.gigachadllc.enchantedtowers;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.maps);
+        assert mapFragment != null;
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+
     }
 }
