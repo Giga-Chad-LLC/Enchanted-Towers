e7cd01d40fb9de4fe9eee887b5f6395250c6b007
Vladislav Art <vladislav0art@gmail.com>
2023-02-24 23:04:42 +0100

    Create proto model & update server code

---
+++ server/app/src/main/java/HelloWorldServer.java
@@ -0,0 +1,63 @@
+import enchantedtowers.sample.Sample;
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+
+import java.io.IOException;
+
+import io.grpc.stub.StreamObserver;
+
+
+class HelloWorldServiceImpl extends HelloWorldServiceGrpc.HelloWorldServiceImplBase {
+
+    @Override
+    public void hello(
+            HelloWorld.HelloRequest request,
+            StreamObserver<HelloWorld.HelloResponse> responseObserver) {
+        System.out.println(
+                "Handling hello endpoint: " + request.toString());
+
+
+        String text = request.getText() + " World";
+        HelloWorld.HelloResponse response =
+                HelloWorld.HelloResponse.newBuilder()
+                        .setText(text).build();
+
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
+}
+
+
+public class HelloWorldServer {
+    private static final int PORT = 50051;
+    private Server server;
+
+    public void start() throws IOException {
+        server = ServerBuilder.forPort(PORT)
+                .addService(new HelloWorldServiceImpl())
+                .build()
+                .start();
+    }
+
+    public void blockUntilShutdown() throws InterruptedException {
+        if (server == null) {
+            return;
+        }
+
+        server.awaitTermination();
+    }
+
+    public static void main(String[] args)
+            throws InterruptedException, IOException {
+        HelloWorldServer server = new HelloWorldServer();
+        server.start();
+        server.blockUntilShutdown();
+    }
+}
+
+
+/*public class Server {
+    public static void main(String[] args) {
+        Sample.print();
+    }
+}*/
--- server/app/src/main/java/Server.java
+++
@@ -1,8 +0,0 @@
-import enchantedtowers.sample.Sample;
-
-public class Server {
-    public static void main(String[] args) {
-        Sample.print();
-//        System.out.println("Hello, World!");
-    }
-}
