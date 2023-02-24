cb35784a5e58c878a7048893976dd572a89914fa
Vladislav Art <vladislav0art@gmail.com>
2023-02-24 23:09:06 +0100

    Add dependancy on protobuf & gprc to client:app project
    Cannot make client:app see the generated from proto models
    grpc and java classes: during `compileDebugJavaWithJavac` task error
    "package `HelloWorldServiceGrpc` does not exist" occurs. But this
    package is located in `common/utils/build/..generated/../grpc` folder
    and have been included into build (IDE recognises it but gradle does not
    include it).
    
    Problem may be related to this:
    https://issuetracker.google.com/issues/37005594?pli=1

--- client/app/src/main/java/com/example/simple_server_app/MainActivity.java
+++ client/app/src/main/java/com/example/simple_server_app/MainActivity.java
@@ -5,10 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import enchantedtowers.sample.Sample;
 
+import io.grpc.Grpc;
+import io.grpc.StatusRuntimeException;
 
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        HelloWorldServiceGrpc.HelloWorldServiceBlockingStub blockingStub;
+
         Sample.print();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
