52d4bfe34a49b916c62ef83d7e4d476ac94124a1
Vladislav Art <vladislav0art@gmail.com>
2023-02-27 11:52:53 +0100

    Rewrite HelloWorldServer to use updated proto models

--- server/app/src/main/java/HelloWorldServer.java
+++ server/app/src/main/java/HelloWorldServer.java
@@ -1,65 +1,93 @@
-import enchantedtowers.sample.Sample;
-import io.grpc.Server;
 import io.grpc.ServerBuilder;
-
-import java.io.IOException;
-
+import io.grpc.Grpc;
+import io.grpc.InsecureServerCredentials;
+import io.grpc.Server;
 import io.grpc.stub.StreamObserver;
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import enchantedtowers.sample.Sample;
+// generated
+import generated.proto.java.HelloWorld.HelloRequest;
+import generated.proto.java.HelloWorld.HelloReply;
+import  generated.proto.java.GreeterGrpc;
 
 
-class HelloWorldServiceImpl extends HelloWorldServiceGrpc.HelloWorldServiceImplBase {
-
-    @Override
-    public void hello(
-            HelloWorld.HelloRequest request,
-            StreamObserver<HelloWorld.HelloResponse> responseObserver) {
-        System.out.println(
-                "Handling hello endpoint: " + request.toString());
-
-
-        String text = request.getText() + " World";
-        HelloWorld.HelloResponse response =
-                HelloWorld.HelloResponse.newBuilder()
-                        .setText(text).build();
-
-        responseObserver.onNext(response);
-        responseObserver.onCompleted();
-    }
-}
-
-
+/**
+ * Server that manages startup/shutdown of a {@code Greeter} server.
+ */
 public class HelloWorldServer {
-    private static final int PORT = 50051;
+    private static final Logger logger = Logger.getLogger(HelloWorldServer.class.getName());
+
     private Server server;
 
-    public void start() throws IOException {
-        server = ServerBuilder.forPort(PORT)
-                .addService(new HelloWorldServiceImpl())
+    private void start() throws IOException {
+        /* The port on which the server should run */
+        int port = 50051;
+        server = Grpc.newServerBuilderForPort(port, InsecureServerCredentials.create())
+                .addService(new GreeterImpl())
                 .build()
                 .start();
-        System.out.println("Server started on port " + server.getPort());
+        logger.info("Server started, listening on " + port);
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            @Override
+            public void run() {
+                // Use stderr here since the logger may have been reset by its JVM shutdown hook.
+                System.err.println("*** shutting down gRPC server since JVM is shutting down");
+                try {
+                    HelloWorldServer.this.stop();
+                } catch (InterruptedException e) {
+                    e.printStackTrace(System.err);
+                }
+                System.err.println("*** server shut down");
+            }
+        });
     }
 
-    public void blockUntilShutdown() throws InterruptedException {
-        if (server == null) {
-            return;
+    private void stop() throws InterruptedException {
+        if (server != null) {
+            server.shutdown().awaitTermination(30, TimeUnit.SECONDS);
         }
-
-        server.awaitTermination();
     }
 
-    public static void main(String[] args)
-            throws InterruptedException, IOException {
-        Sample.print();
-        HelloWorldServer server = new HelloWorldServer();
+    /**
+     * Await termination on the main thread since the grpc library uses daemon threads.
+     */
+    private void blockUntilShutdown() throws InterruptedException {
+        if (server != null) {
+            server.awaitTermination();
+        }
+    }
+
+    /**
+     * Main launches the server from the command line.
+     */
+    public static void main(String[] args) throws IOException, InterruptedException {
+        final HelloWorldServer server = new HelloWorldServer();
         server.start();
         server.blockUntilShutdown();
     }
+
+    static class GreeterImpl extends GreeterGrpc.GreeterImplBase {
+        @Override
+        public void sayHello(HelloRequest req, StreamObserver<HelloReply> responseObserver) {
+            logger.info("Got request. Name: '" + req.getName() + "'");
+            HelloReply reply = HelloReply.newBuilder().setMessage("Hello " + req.getName()).build();
+            responseObserver.onNext(reply);
+            responseObserver.onCompleted();
+        }
+    }
 }
 
 
-/*public class HelloWorldServer {
+
+
+/*
+public class HelloWorldServer {
     public static void main(String[] args) {
         Sample.print();
     }
-}*/
+}
+*/
