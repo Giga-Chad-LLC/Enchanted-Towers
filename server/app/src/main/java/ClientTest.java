59865486c4c598b1c9168a1b490bf4254a686e1b
Vladislav Art <vladislav0art@gmail.com>
2023-03-05 01:16:57 +0100

    Create attackTower method in ClientTest that makes corresponding rpc call

--- server/app/src/main/java/ClientTest.java
+++ server/app/src/main/java/ClientTest.java
@@ -1,3 +1,4 @@
+import enchantedtowers.common.utils.proto.responses.EnchantmentResponse;
 import io.grpc.Channel;
 import io.grpc.Grpc;
 import io.grpc.InsecureChannelCredentials;
@@ -5,14 +6,19 @@ import io.grpc.ManagedChannel;
 import io.grpc.StatusRuntimeException;
 
 import java.util.Iterator;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-// proto
+// requests
 import enchantedtowers.common.utils.proto.requests.PlayerCoordinatesRequest;
+import enchantedtowers.common.utils.proto.requests.TowerAttackRequest;
+// responses
 import enchantedtowers.common.utils.proto.responses.TowerResponse;
 import enchantedtowers.common.utils.proto.responses.TowersAggregationResponse;
+import enchantedtowers.common.utils.proto.responses.AttackTowerResponse;
+// services
 import enchantedtowers.common.utils.proto.services.TowersServiceGrpc;
 
 
@@ -37,6 +43,35 @@ public class ClientTest {
         }
     }
 
+    public void attackTower() {
+        TowerAttackRequest.Builder requestBuilder = TowerAttackRequest.newBuilder();
+        requestBuilder.setTowerId(0);
+        requestBuilder.getPlayerCoordinatesBuilder().setX(0).setY(0);
+
+        TowerAttackRequest request = requestBuilder.build();
+
+        try {
+            AttackTowerResponse response = blockingStub.attackTower(request);
+            if (response.getError().getHasError()) {
+                logger.log(Level.INFO, "Error[ type="
+                        + response.getError().getType().name()
+                        + ", message='" + response.getError().getMessage()
+                        + "']");
+            }
+            else {
+                List<EnchantmentResponse> enchantments = response.getEnchantments().getEnchantmentsList();
+
+                for (var enchantment : enchantments) {
+                    // create Enchantment game class
+                    logger.log(Level.INFO, "EnchantmentResponse[ element=" + enchantment.getElement().name() + "]");
+                }
+            }
+        }
+        catch(StatusRuntimeException err) {
+            logger.log(Level.WARNING, "RPC failed: {0}", err.getStatus());
+        }
+    }
+
     public static void main(String[] args) throws Exception {
         String target = "localhost:50051";
         ManagedChannel channel = Grpc.newChannelBuilder(target, InsecureChannelCredentials.create()).build();
@@ -44,6 +79,7 @@ public class ClientTest {
         try {
             ClientTest client = new ClientTest(channel);
             client.getTowersCoordinates();
+            client.attackTower();
         }
         finally {
             channel.shutdownNow().awaitTermination(5, TimeUnit.SECONDS);
