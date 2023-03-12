3717a7030e62bc610c2b74fbaf51f9e45921afc0
Dmitrii Art <artuhovdmitrii@gmail.com>
2023-03-12 21:33:26 +0100

    Include open-cv in client app

--- client/app/src/main/java/enchantedtowers/client/MainActivity.java
+++ client/app/src/main/java/enchantedtowers/client/MainActivity.java
@@ -5,9 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.StrictMode;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import org.opencv.android.OpenCVLoader;
 
 import io.grpc.Channel;
 import io.grpc.Grpc;
@@ -116,8 +119,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        // Logging to logcat the opencv initialization status
+        Log.d("OPENCV", "OpenCV init status: " + OpenCVLoader.initDebug());
+
         setContentView(R.layout.activity_main);
-//
+
 //        /*StrictMode.setVmPolicy(new StrictMode.VmPolicy.Builder(StrictMode.getVmPolicy())
 //                .detectLeakedClosableObjects()
 //                .build());*/
