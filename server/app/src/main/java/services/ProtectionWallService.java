2d59b5b301d177d0c7378d70b300e08424733cef
Vladislav Art <vladislav0art@gmail.com>
2023-03-13 20:13:25 +0100

    Create boilerplate of ProtectionWallService

---
+++ server/app/src/main/java/services/ProtectionWallService.java
@@ -0,0 +1,31 @@
+package services;
+
+import io.grpc.stub.StreamObserver;
+
+// interactors
+
+// proto/common
+import enchantedtowers.common.utils.proto.common.ProtectionWallModel;
+// requests
+import enchantedtowers.common.utils.proto.requests.ProtectionWallsRequest;
+import enchantedtowers.common.utils.proto.requests.ProtectionWallSetupRequest;
+// responses
+import enchantedtowers.common.utils.proto.responses.ProtectionWallsAggregationResponse;
+// services
+import enchantedtowers.common.utils.proto.services.ProtectionWallServiceGrpc;
+
+
+
+public class ProtectionWallService extends ProtectionWallServiceGrpc.ProtectionWallServiceImplBase {
+    @Override
+    public void getTowerProtectionWalls(ProtectionWallsRequest request,
+                                        StreamObserver<ProtectionWallsAggregationResponse> responseObserver) {
+        // TODO: implement
+    }
+
+    @Override
+    public void setUpProtectionWallOnTower(ProtectionWallSetupRequest request,
+                                           StreamObserver<ProtectionWallModel> responseObserver) {
+        // TODO: implement
+    }
+}
