7f8e7ce8d535acc8fd0d3c5e61ca46559e17c2a3
Vladislav Art <vladislav0art@gmail.com>
2023-03-18 19:04:11 +0100

    Create blank fragment (boilerplate for MapFragment)

---
+++ client/app/src/main/java/enchantedtowers/client/MapFragment.java
@@ -0,0 +1,45 @@
+package enchantedtowers.client;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class MapFragment extends Fragment {
+    public MapFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @return A new instance of fragment MapFragment.
+     */
+    public static MapFragment newInstance() {
+        MapFragment fragment = new MapFragment();
+         Bundle args = new Bundle();
+         fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_map, container, false);
+    }
+}
