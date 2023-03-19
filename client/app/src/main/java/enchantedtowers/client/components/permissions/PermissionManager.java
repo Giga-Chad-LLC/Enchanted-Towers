d4315d2d97dece47bff0c6a51e4a328e1bef27ce
Vladislav Art <vladislav0art@gmail.com>
2023-03-19 19:15:11 +0100

    Create hook-methods to conform proper permission workflow in PermissionManager

--- client/app/src/main/java/enchantedtowers/client/components/permissions/PermissionManager.java
+++ client/app/src/main/java/enchantedtowers/client/components/permissions/PermissionManager.java
@@ -7,7 +7,39 @@ import android.content.pm.PackageManager;
 import androidx.annotation.NonNull;
 import androidx.core.content.ContextCompat;
 
+
 public class PermissionManager {
+    private boolean withPermissionsCalled = false;
+    private boolean allPermissionsGranted = false;
+
+
+    public PermissionManager withPermissions(String[] requiredPermissions, Context context, Runnable callback) {
+        boolean allPermissionsGranted = true;
+        for (String permission : requiredPermissions) {
+            if (ContextCompat.checkSelfPermission(context, permission) == PackageManager.PERMISSION_DENIED) {
+                allPermissionsGranted = false;
+                break;
+            }
+        }
+
+        this.withPermissionsCalled = true;
+        this.allPermissionsGranted = allPermissionsGranted;
+
+        if (allPermissionsGranted) {
+            callback.run();
+        }
+
+        return this;
+    }
+
+    public void otherwise(Runnable callback) throws IllegalStateException {
+        if (!withPermissionsCalled) {
+            throw new IllegalStateException("'withPermissions' must be called before 'otherwise'");
+        }
+        if (!allPermissionsGranted) {
+            callback.run();
+        }
+    }
     /**
      * Returns {@code true} if either of
      *      {@link Manifest.permission#ACCESS_FINE_LOCATION} or
