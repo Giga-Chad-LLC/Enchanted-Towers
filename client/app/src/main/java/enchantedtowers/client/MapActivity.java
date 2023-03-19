f6a0683491d970f98ede49724278867070b26c11
Vladislav Art <vladislav0art@gmail.com>
2023-03-19 12:49:50 +0100

    Create core workflow of accessing location permissions in MapActivity

--- client/app/src/main/java/enchantedtowers/client/MapActivity.java
+++ client/app/src/main/java/enchantedtowers/client/MapActivity.java
@@ -1,8 +1,13 @@
 package enchantedtowers.client;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 
@@ -12,14 +17,72 @@ public class MapActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
 
+        ActivityResultLauncher<String[]> locationPermissionRequest =
+                registerForActivityResult(new ActivityResultContracts.RequestMultiplePermissions(), result -> {
+                    System.out.println("INFO: " + result);
+
+                    Boolean fineLocationGranted = result.getOrDefault(
+                            Manifest.permission.ACCESS_FINE_LOCATION, false);
+
+                    Boolean coarseLocationGranted = result.getOrDefault(
+                            Manifest.permission.ACCESS_COARSE_LOCATION,false);
+
+                    assert fineLocationGranted != null;
+                    assert coarseLocationGranted != null;
+
+                    if (/*fineLocationGranted != null &&*/ fineLocationGranted) {
+                        System.out.println("INFO: fineLocationGranted");
+                        mountGoogleMapsFragment();
+                    }
+                    else if (/*coarseLocationGranted != null &&*/ coarseLocationGranted) {
+                        System.out.println("INFO: coarseLocationGranted");
+                    }
+                    else {
+                        System.out.println("INFO: No location access granted");
+                    }
+                }
+        );
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) ==
+                PackageManager.PERMISSION_GRANTED &&
+                ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) ==
+                        PackageManager.PERMISSION_GRANTED) {
+            // You can use the API that requires the permission.
+            System.out.println("INFO: ACCESS_FINE_LOCATION & ACCESS_COARSE_LOCATION granted");
+        } else if (shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION) ||
+                shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_COARSE_LOCATION)) {
+            // In an educational UI, explain to the user why your app requires this
+            // permission for a specific feature to behave as expected, and what
+            // features are disabled if it's declined. In this UI, include a
+            // "cancel" or "no thanks" button that lets the user continue
+            // using your app without granting the permission.
+            boolean rationalFineLocation = shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION);
+            boolean rationalCoarseLocation = shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_COARSE_LOCATION);
+            System.out.println("INFO: rationalFineLocation=" + rationalFineLocation + ", rationalCoarseLocation=" + rationalCoarseLocation);
+
+            locationPermissionRequest.launch(new String[] {
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+            });
+        } else {
+            // You can directly ask for the permission.
+            // The registered ActivityResultCallback gets the result of this request.
+            locationPermissionRequest.launch(new String[] {
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            });
+        }
+    }
+
+    private void mountGoogleMapsFragment() {
         // create fragment
         Fragment mapFragment = MapFragment.newInstance();
 
         // mount fragment into layout
         getSupportFragmentManager()
-            .beginTransaction()
-            .add(R.id.map_frame_layout, mapFragment)
-            .commit();
+                .beginTransaction()
+                .add(R.id.map_frame_layout, mapFragment)
+                .commit();
     }
 
 }
