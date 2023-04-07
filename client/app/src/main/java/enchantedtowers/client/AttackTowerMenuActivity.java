1a78e75181f49f7636c095c719c22bc950ec8d6a
Vladislav Art <vladislav0art@gmail.com>
2023-04-07 22:14:26 +0200

    Create async stub request in android client

--- client/app/src/main/java/enchantedtowers/client/AttackTowerMenuActivity.java
+++ client/app/src/main/java/enchantedtowers/client/AttackTowerMenuActivity.java
@@ -1,34 +1,27 @@
 package enchantedtowers.client;
 
 import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import io.grpc.Channel;
+import enchantedtowers.common.utils.proto.requests.TowerAttackRequest;
+import enchantedtowers.common.utils.proto.responses.ActionResultResponse;
+import enchantedtowers.common.utils.proto.services.TowerAttackServiceGrpc;
 import io.grpc.Grpc;
 import io.grpc.InsecureChannelCredentials;
 import io.grpc.ManagedChannel;
-import io.grpc.StatusRuntimeException;
-
-// responses
-import enchantedtowers.common.utils.proto.responses.ActionResultResponse;
-// requests
-import enchantedtowers.common.utils.proto.requests.TowerAttackRequest;
-// services
-import enchantedtowers.common.utils.proto.services.TowerAttackServiceGrpc;
-
+import io.grpc.ManagedChannelBuilder;
+import io.grpc.stub.StreamObserver;
 
 
 public class AttackTowerMenuActivity extends AppCompatActivity {
+    TowerAttackServiceGrpc.TowerAttackServiceStub asyncStub;
+    ManagedChannel channel;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_attack_tower_menu);
-
-        String target = "localhost:8080";// "10.0.2.2:50051";
-        // Grpc.newChannelBuilderForAddress("localhost", 50051, InsecureChannelCredentials.create());
-
-        ManagedChannel channel = Grpc.newChannelBuilder(target, InsecureChannelCredentials.create()).build();
-        TowerAttackServiceGrpc.TowerAttackServiceBlockingStub blockingStub = TowerAttackServiceGrpc.newBlockingStub(channel);
 
         TowerAttackRequest.Builder requestBuilder = TowerAttackRequest.newBuilder();
         requestBuilder.setTowerId(0);
@@ -36,7 +29,37 @@ public class AttackTowerMenuActivity extends AppCompatActivity {
                 .setPlayerId(0)
                 .build();
 
-        ActionResultResponse response = blockingStub.attackTowerById(requestBuilder.build());
-        System.out.println("Result: " + response.getSuccess());
+        /*
+        "10.0.2.2:8080"  - emulators
+        "localhost:8080" - android device
+        */
+        String target = "10.0.2.2:8080";
+
+        System.out.println("target: " + target);
+
+        // Grpc.newChannelBuilderForAddress("localhost", 50051, InsecureChannelCredentials.create());
+        channel = Grpc.newChannelBuilder(target, InsecureChannelCredentials.create()).build();
+        asyncStub = TowerAttackServiceGrpc.newStub(channel);
+
+        asyncStub.attackTowerById(requestBuilder.build(), new StreamObserver<ActionResultResponse>() {
+            @Override
+            public void onNext(ActionResultResponse response) {
+                // Handle the response
+                System.out.println("attackTowerById::Received response: " + response.getSuccess());
+            }
+
+            @Override
+            public void onError(Throwable t) {
+                // Handle the error
+                System.err.println("attackTowerById::Error: " + t.getMessage());
+            }
+
+            @Override
+            public void onCompleted() {
+                // Handle the completion
+                System.out.println("attackTowerById::Completed");
+            }
+        });
+
     }
 }
