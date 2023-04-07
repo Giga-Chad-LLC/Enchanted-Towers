5a9173cf064ef1827136536a6440a656b30a1b72
Vladislav Art <vladislav0art@gmail.com>
2023-04-07 16:31:43 +0200

    Create AttackTowerMenuActivity activity

---
+++ client/app/src/main/java/enchantedtowers/client/AttackTowerMenuActivity.java
@@ -0,0 +1,14 @@
+package enchantedtowers.client;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AttackTowerMenuActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_attack_tower_menu);
+    }
+}
