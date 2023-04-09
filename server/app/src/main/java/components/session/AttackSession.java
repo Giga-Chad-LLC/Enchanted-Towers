c3e04eca958c5db03b8b4fe804c14a71f004a506
Dmitrii Art <artuhovdmitrii@gmail.com>
2023-04-09 21:41:17 +0200

    Add spectator related methods to AttackSession

--- server/app/src/main/java/components/session/AttackSession.java
+++ server/app/src/main/java/components/session/AttackSession.java
@@ -1,15 +1,18 @@
 package components.session;
 
 import enchantedtowers.common.utils.proto.requests.TowerAttackRequest;
+import enchantedtowers.common.utils.proto.responses.SpectateTowerAttackResponse;
 import enchantedtowers.game_logic.HausdorffMetric;
 import enchantedtowers.game_logic.SpellsPatternMatchingAlgorithm;
 import enchantedtowers.game_models.Spell;
 import enchantedtowers.game_models.SpellBook;
 import enchantedtowers.game_models.utils.Utils;
 import enchantedtowers.game_models.utils.Vector2;
+import io.grpc.stub.StreamObserver;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.logging.Logger;
 
 
 public class AttackSession {
@@ -23,25 +26,28 @@ public class AttackSession {
 
     private final List<SpellsPatternMatchingAlgorithm.MatchedTemplateDescription> drawnSpellsDescriptions = new ArrayList<>();
     private final List<Integer> drawnSpellColors = new ArrayList<>();
-    private final List<Integer> canvasViewer = new ArrayList<>();
+    private final List<Spectator> spectators = new ArrayList<>();
+
+    private static final Logger logger = Logger.getLogger(AttackSession.class.getName());
 
     AttackSession(int attackingPlayerId, int attackedTowerId) {
         this.attackingPlayerId = attackingPlayerId;
         this.attackedTowerId = attackedTowerId;
     }
 
+    public record Spectator(int playerId, StreamObserver<SpectateTowerAttackResponse> streamObserver) {}
+
     public static AttackSession fromRequest(final TowerAttackRequest request) {
         return new AttackSession(request.getPlayerData().getPlayerId(), request.getTowerId());
-    }
-
-    public void addPointToCurrentSpell(Vector2 point) {
-        currentSpellPoints.add(point);
     }
 
     public int getAttackingPlayerId() {
         return attackingPlayerId;
     }
 
+    public int getAttackedTowerId() {
+        return attackedTowerId;
+    }
 
     public int getCurrentSpellColorId() {
         return currentSpellColorId.get();
@@ -49,6 +55,10 @@ public class AttackSession {
 
     public void setCurrentSpellColorId(int currentSpellColorId) {
         this.currentSpellColorId = Optional.of(currentSpellColorId);
+    }
+
+    public void addPointToCurrentSpell(Vector2 point) {
+        currentSpellPoints.add(point);
     }
 
     public void clearCurrentDrawing() {
@@ -92,5 +102,26 @@ public class AttackSession {
         }
 
         return Optional.empty();
+    }
+
+    public void addSpectator(int playerId, StreamObserver<SpectateTowerAttackResponse> streamObserver) {
+        spectators.add(new Spectator(playerId, streamObserver));
+    }
+
+    public Spectator pollSpectatorById(int playerId) {
+        var iterator = spectators.iterator();
+        Spectator removedSpectator = null;
+
+        while (iterator.hasNext()) {
+            Spectator spectator = iterator.next();
+            if (spectator.playerId == playerId) {
+                logger.info("Spectator with id '" + playerId + "' removed from session");
+                removedSpectator = spectator;
+                iterator.remove();
+                break;
+            }
+        }
+
+        return removedSpectator;
     }
 }
