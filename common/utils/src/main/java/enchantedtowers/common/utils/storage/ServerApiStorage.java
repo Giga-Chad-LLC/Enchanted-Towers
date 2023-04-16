3809e8673923befcca6e6accfc5af2ca829b342d
Vladislav Art <vladislav0art@gmail.com>
2023-04-16 11:52:44 +0200

    Add description of ServerApiStorage server & client hosts

--- common/utils/src/main/java/enchantedtowers/common/utils/storage/ServerApiStorage.java
+++ common/utils/src/main/java/enchantedtowers/common/utils/storage/ServerApiStorage.java
@@ -18,10 +18,13 @@ public class ServerApiStorage {
      * Available endpoints:
      * <ol>
      *  <li><b>10.0.2.2:8080</b> - use for emulators</li>
-     *  <li><b>localhost:8080</b> - use for android device</li>
-     *  <li><b>192.168.0.103:8080</b> - use for Wi-Fi LAN (if server must listen to <b>192.168.0.103:8080</b>)</li>
+     *  <li>
+     *      <b>localhost:8080</b> - use for android device (use <code>adb reverse tcp:8080 tcp:8080</code> for port mapping)
+     *      See: <a href="https://trello.com/c/ZAte5H6T/47-port-mapping-%D0%BC%D0%B5%D0%B6%D0%B4%D1%83-android-%D1%83%D1%81%D1%82%D1%80%D0%BE%D0%B9%D1%81%D1%82%D0%B2%D0%BE%D0%BC-%D0%B8-%D0%BF%D0%BA">Trello card about port-mapping</a>
+     *  </li>
+     *  <li><b>192.168.0.103:8080</b> - use for Wi-Fi LAN (server must listen to <b>192.168.0.103:8080</b>)</li>
      * </ol>
-    */
+     */
     private ServerApiStorage() {
         serverHost = "localhost";
         clientHost = "10.0.2.2"; // emulators
