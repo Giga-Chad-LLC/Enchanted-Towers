3926024f0c1beaac2c34e72dc3ed3c65019a74ea
Vladislav Art <vladislav0art@gmail.com>
2023-04-24 13:18:08 +0200

    Move proto models into separate folders & remove obsolete interactors and proto-files (everything related to Enchantments)

--- server/app/src/main/java/services/TowersService.java
+++ server/app/src/main/java/services/TowersService.java
@@ -4,13 +4,10 @@ import io.grpc.stub.StreamObserver;
 
 // interactors
 import interactors.CreateTowersResponseInteractor;
-import interactors.AttackTowerResponseInteractor;
 // requests
 import enchantedtowers.common.utils.proto.requests.PlayerCoordinatesRequest;
-import enchantedtowers.common.utils.proto.requests.TowerAttackRequest;
 // responses
 import enchantedtowers.common.utils.proto.responses.TowersAggregationResponse;
-import enchantedtowers.common.utils.proto.responses.AttackTowerResponse;
 // services
 import enchantedtowers.common.utils.proto.services.TowersServiceGrpc;
 
@@ -22,19 +19,6 @@ public class TowersService extends TowersServiceGrpc.TowersServiceImplBase {
 
         CreateTowersResponseInteractor interactor = new CreateTowersResponseInteractor();
         TowersAggregationResponse response = interactor.execute(request);
-
-        responseObserver.onNext(response);
-        responseObserver.onCompleted();
-    }
-
-    @Override
-    public void attackTower(TowerAttackRequest request, StreamObserver<AttackTowerResponse> responseObserver) {
-        System.out.println("attackTower:\tTowerAttackRequest[towerId=" + request.getTowerId()
-                + ", playerX=" + request.getPlayerCoordinates().getX()
-                + ", playerY=" + request.getPlayerCoordinates().getY() + "]");
-
-        AttackTowerResponseInteractor interactor = new AttackTowerResponseInteractor();
-        AttackTowerResponse response = interactor.execute(request);
 
         responseObserver.onNext(response);
         responseObserver.onCompleted();
