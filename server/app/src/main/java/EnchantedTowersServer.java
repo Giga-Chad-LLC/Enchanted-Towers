8bd69afce6e1b099111e3e225ed180254c3cc7b4
Vladislav Art <vladislav0art@gmail.com>
2023-05-01 16:38:28 +0200

    Optimize imports in ProtectionWallService + refactor code in server class

--- server/app/src/main/java/EnchantedTowersServer.java
+++ server/app/src/main/java/EnchantedTowersServer.java
@@ -35,19 +35,16 @@ public class EnchantedTowersServer {
                 .start();
 
         logger.info("Server started: host='" + host + "', port='" + port + "'");
-        Runtime.getRuntime().addShutdownHook(new Thread() {
-            @Override
-            public void run() {
-                // Use stderr here since the logger may have been reset by its JVM shutdown hook.
-                System.err.println("*** shutting down gRPC server since JVM is shutting down");
-                try {
-                    EnchantedTowersServer.this.stop();
-                } catch (InterruptedException e) {
-                    e.printStackTrace(System.err);
-                }
-                System.err.println("*** server shut down");
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            // Use stderr here since the logger may have been reset by its JVM shutdown hook.
+            System.err.println("*** shutting down gRPC server since JVM is shutting down");
+            try {
+                EnchantedTowersServer.this.stop();
+            } catch (InterruptedException e) {
+                e.printStackTrace(System.err);
             }
-        });
+            System.err.println("*** server shut down");
+        }));
     }
 
     private void stop() throws InterruptedException {
