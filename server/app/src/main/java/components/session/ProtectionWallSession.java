8baf0f29be339ee9d936067271231061757f5d28
Vladislav Art <vladislav0art@gmail.com>
2023-05-01 15:17:14 +0200

    Implement basic methods of ProtectionWallSession and its manager

--- server/app/src/main/java/components/session/ProtectionWallSession.java
+++ server/app/src/main/java/components/session/ProtectionWallSession.java
@@ -1,4 +1,48 @@
 package components.session;
 
+import enchantedtowers.common.utils.proto.responses.SessionInfoResponse;
+import io.grpc.stub.StreamObserver;
+
+import java.util.function.IntConsumer;
+
 public class ProtectionWallSession {
+    private final int id;
+    private final int playerId;
+    private final int towerId;
+    private final StreamObserver<SessionInfoResponse> playerResponseObserver;
+    // TODO: keep track of canvas state
+
+    private final IntConsumer onSessionExpiredCallback;
+    // this lock object is used as mutual exclusion lock
+    private final Object lock = new Object();
+
+    ProtectionWallSession(int id,
+                          int playerId,
+                          int towerId,
+                          StreamObserver<SessionInfoResponse> playerResponseObserver,
+                          IntConsumer onSessionExpiredCallback) {
+        this.id = id;
+        this.playerId = playerId;
+        this.towerId = towerId;
+        this.playerResponseObserver = playerResponseObserver;
+        this.onSessionExpiredCallback = onSessionExpiredCallback;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getPlayerId() {
+        return playerId;
+    }
+
+    public int getTowerId() {
+        return towerId;
+    }
+
+    public StreamObserver<SessionInfoResponse> getPlayerResponseObserver() {
+        return playerResponseObserver;
+    }
+
+
 }
--- server/app/src/main/java/components/session/ProtectionWallSessionManager.java
+++ server/app/src/main/java/components/session/ProtectionWallSessionManager.java
@@ -1,4 +1,64 @@
 package components.session;
 
+import enchantedtowers.common.utils.proto.responses.SessionInfoResponse;
+import io.grpc.stub.StreamObserver;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+import java.util.function.IntConsumer;
+
 public class ProtectionWallSessionManager {
+    // towerId -> ProtectionWallSession
+    private final Map<Integer, ProtectionWallSession> sessions = new HashMap<>();
+    private int CURRENT_SESSION_ID = 0;
+    private final Object lock = new Object();
+
+
+    public ProtectionWallSession createSession(int playerId,
+                                               int towerId,
+                                               StreamObserver<SessionInfoResponse> playerResponseObserver,
+                                               IntConsumer onSessionExpiredCallback) {
+        synchronized (lock) {
+            var session = new ProtectionWallSession(
+                    CURRENT_SESSION_ID++, playerId, towerId, playerResponseObserver, onSessionExpiredCallback);
+
+            sessions.put(towerId, session);
+            return session;
+        }
+    }
+
+    public void remove(ProtectionWallSession session) {
+        synchronized (lock) {
+            boolean removed = sessions.remove(session.getTowerId(), session);
+            if (!removed) {
+                throw new NoSuchElementException("Attack session with id " + session.getId() + " not found");
+            }
+        }
+    }
+
+    public boolean hasSessionAssociatedWithPlayerId(int playerId) {
+        synchronized (lock) {
+            for (var session : sessions.values()) {
+                if (playerId == session.getPlayerId()) {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+
+    public Optional<ProtectionWallSession> getSessionById(int sessionId) {
+        synchronized (lock) {
+            for (var session : sessions.values()) {
+                if (sessionId == session.getId()) {
+                    return Optional.of(session);
+                }
+            }
+            return Optional.empty();
+        }
+    }
+
+
 }
