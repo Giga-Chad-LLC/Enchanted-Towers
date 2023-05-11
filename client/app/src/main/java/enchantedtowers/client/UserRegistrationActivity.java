c3b726bda40343d29212711108f8f1b833d1cd44
dezd4r <naumkinvs03@gmail.com>
2023-05-11 17:09:57 +0200

    adding a registration page without implementing registration

--- client/app/src/main/java/enchantedtowers/client/UserRegistrationActivity.java
+++ client/app/src/main/java/enchantedtowers/client/UserRegistrationActivity.java
@@ -1,19 +1,25 @@
 package enchantedtowers.client;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class UserRegistrationActivity extends AppCompatActivity {
-    private Button buttonSignIn;
-    private Button buttonRegistration;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+    }
 
+    public void sendUserData(View view) {
+        if (view.getId() == R.id.buttonToSendData) {
+            System.err.println("implement button");
+        } else {
+            System.err.println("Unknown view emitted `MainActivity::changeActivity`: " + view);
+        }
     }
 }
