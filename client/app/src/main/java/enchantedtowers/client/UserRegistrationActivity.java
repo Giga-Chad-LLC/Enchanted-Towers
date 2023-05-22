1027d8f617e98cf526e11da7c54ff4105963dd22
dezd4r <naumkinvs03@gmail.com>
2023-05-22 19:44:44 +0200

    Implement register page

--- client/app/src/main/java/enchantedtowers/client/UserRegistrationActivity.java
+++ client/app/src/main/java/enchantedtowers/client/UserRegistrationActivity.java
@@ -4,11 +4,24 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class UserRegistrationActivity extends AppCompatActivity {
+import enchantedtowers.common.utils.proto.requests.RegistrationRequest;
+import enchantedtowers.common.utils.proto.responses.ActionResultResponse;
+import enchantedtowers.common.utils.proto.services.AuthServiceGrpc;
+import enchantedtowers.common.utils.storage.ServerApiStorage;
+import io.grpc.Grpc;
+import io.grpc.InsecureChannelCredentials;
+import io.grpc.ManagedChannel;
+import io.grpc.stub.StreamObserver;
 
+public class UserRegistrationActivity extends AppCompatActivity {
+    private AuthServiceGrpc.AuthServiceStub authServiceStub;
+    //private AuthServiceGrpc.AuthServiceBlockingStub authStub;
+    private ManagedChannel channel;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,10 +29,55 @@ public class UserRegistrationActivity extends AppCompatActivity {
     }
 
     public void sendUserData(View view) {
-        if (view.getId() == R.id.buttonToSendData) {
-            System.err.println("implement button");
-        } else {
-            System.err.println("Unknown view emitted `MainActivity::changeActivity`: " + view);
-        }
+
+        String host = ServerApiStorage.getInstance().getClientHost();
+        int port = ServerApiStorage.getInstance().getPort();
+        channel = Grpc.newChannelBuilderForAddress(host, port, InsecureChannelCredentials.create()).build();
+        //authStub = AuthServiceGrpc.newBlockingStub(channel);
+        authServiceStub = AuthServiceGrpc.newStub(channel);
+
+        EditText userNameTextInput = findViewById(R.id.editTextPersonName);
+        EditText userPasswordTextInput = findViewById(R.id.editTextPassword);
+        EditText userEmailTextInput = findViewById(R.id.editTextEmailAddress);
+
+        RegistrationRequest request = RegistrationRequest.newBuilder()
+                .setUsername(userNameTextInput.getText().toString())
+                .setPassword(userPasswordTextInput.getText().toString())
+                .setEmail(userEmailTextInput.getText().toString())
+                .build();
+
+        //try {
+
+        authServiceStub.register(request, new StreamObserver<>() {
+            @Override
+            public void onNext(ActionResultResponse value) {
+                if (value.hasError()) {
+                    Toast.makeText(UserRegistrationActivity.this, value.getError().getMessage(), Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(UserRegistrationActivity.this, "You are registered", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onError(Throwable t) {
+
+            }
+
+            @Override
+            public void onCompleted() {
+                Toast.makeText(UserRegistrationActivity.this, "You are registered", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+            /*ActionResultResponse response = authStub.register(request);
+            if (response.hasError()) {
+                Toast.makeText(this, response.getError().getMessage(), Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "you registered", Toast.LENGTH_SHORT).show();
+            }*/
+        /*} catch (Exception err) {
+            System.out.println(err.getMessage());
+            Toast.makeText(this, err.getMessage(), Toast.LENGTH_SHORT).show();
+        }*/
     }
 }
--- server/app/src/main/java/services/AuthService.java
+++ server/app/src/main/java/services/AuthService.java
@@ -1,22 +1,87 @@
 package services;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import enchantedtowers.common.utils.proto.requests.LoginRequest;
 import enchantedtowers.common.utils.proto.requests.LogoutRequest;
 import enchantedtowers.common.utils.proto.requests.RegistrationRequest;
 import enchantedtowers.common.utils.proto.responses.ActionResultResponse;
 import enchantedtowers.common.utils.proto.responses.LoginResponse;
+import enchantedtowers.common.utils.proto.responses.ServerError;
 import enchantedtowers.common.utils.proto.services.AuthServiceGrpc;
 import io.grpc.stub.StreamObserver;
 
 public class AuthService extends AuthServiceGrpc.AuthServiceImplBase {
+    private final Logger logger = Logger.getLogger(AuthService.class.getName());
+    class User {
+        String userName;
+        String password;
+        String eMail;
+    }
+
+    List<User> users = new ArrayList<>();
+
     @Override
     public void register(RegistrationRequest request, StreamObserver<ActionResultResponse> responseObserver) {
-        // TODO: implement
+        User newUser = new User();
+
+        newUser.eMail = request.getEmail();
+        newUser.userName = request.getUsername();
+        newUser.password = request.getPassword();
+
+        for (var user: users) {
+            if (user.userName.equals(newUser.userName)) {
+                ServerError error = ServerError.newBuilder().setMessage("User already exist").setType(ServerError.ErrorType.INVALID_REQUEST).build();
+                ActionResultResponse response = ActionResultResponse.newBuilder().setError(error).build();
+                responseObserver.onNext(response);
+                responseObserver.onCompleted();
+                logger.log(Level.INFO, "Name of new user already exist");
+                return;
+            }
+
+            if (user.eMail.equals(newUser.eMail)) {
+                ServerError error = ServerError.newBuilder().setMessage("Email already exist").setType(ServerError.ErrorType.INVALID_REQUEST).build();
+                ActionResultResponse response = ActionResultResponse.newBuilder().setError(error).build();
+                responseObserver.onNext(response);
+                logger.log(Level.INFO, "Email of new user already exist");
+                responseObserver.onCompleted();
+                return;
+            }
+        }
+
+        users.add(newUser);
+        logger.log(Level.INFO, "New user(Email: " + newUser.eMail + ", Name: " + newUser.userName + ", Password:" +newUser.password + ")");
+
+        responseObserver.onCompleted();
+
+        // TODO: rewrite
     }
 
     @Override
     public void login(LoginRequest request, StreamObserver<LoginResponse> responseObserver) {
-        // TODO: implement
+        User newUser = new User();
+
+        newUser.eMail = request.getEmail();
+        newUser.password = request.getPassword();
+
+        for (var user: users) {
+            if (user.eMail.equals(newUser.eMail) && user.password.equals(newUser.password)) {
+                //ActionResultResponse response = ActionResultResponse.newBuilder().setSuccess(true).build();
+                LoginResponse response = LoginResponse.newBuilder().build();
+                responseObserver.onNext(response);
+                responseObserver.onCompleted();
+                return;
+            }
+        }
+
+        ServerError error = ServerError.newBuilder().setMessage("User not exist").setType(ServerError.ErrorType.INVALID_REQUEST).build();
+        LoginResponse response = LoginResponse.newBuilder().setError(error).build();
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+        // TODO: rewrite
     }
 
     @Override
