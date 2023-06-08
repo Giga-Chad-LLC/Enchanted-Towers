0747b8670b7a158f0d2c538c0c12cf33c830f3a5
dezd4r <naumkinvs03@gmail.com>
2023-06-08 16:47:13 +0200

    Rewriting AuthService for working with DB

--- server/app/src/main/java/services/AuthService.java
+++ server/app/src/main/java/services/AuthService.java
@@ -1,10 +1,14 @@
 package services;
 
+import org.mindrot.jbcrypt.BCrypt;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import components.db.User;
+import components.db.UsersDataBase;
 import enchantedtowers.common.utils.proto.requests.LoginRequest;
 import enchantedtowers.common.utils.proto.requests.LogoutRequest;
 import enchantedtowers.common.utils.proto.requests.RegistrationRequest;
@@ -16,23 +20,40 @@ import io.grpc.stub.StreamObserver;
 
 public class AuthService extends AuthServiceGrpc.AuthServiceImplBase {
     private final Logger logger = Logger.getLogger(AuthService.class.getName());
-    class User {
-        String userName;
-        String password;
-        String eMail;
-    }
+    private final UsersDataBase usersDataBase = new UsersDataBase();
 
-    List<User> users = new ArrayList<>();
+    //List<User> users = new ArrayList<>();
 
     @Override
     public void register(RegistrationRequest request, StreamObserver<ActionResultResponse> responseObserver) {
         User newUser = new User();
 
-        newUser.eMail = request.getEmail();
-        newUser.userName = request.getUsername();
-        newUser.password = request.getPassword();
+        newUser.setEmail(request.getEmail());
+        newUser.setName(request.getUsername());
+        newUser.setHashedPassword(BCrypt.hashpw(request.getPassword(), BCrypt.gensalt()));
 
-        for (var user: users) {
+        if (usersDataBase.findUserRecordByEmail(newUser.getEmail()) != null) {
+            ServerError error = ServerError.newBuilder().setMessage("User's name already exist").setType(ServerError.ErrorType.INVALID_REQUEST).build();
+            ActionResultResponse response = ActionResultResponse.newBuilder().setError(error).build();
+            responseObserver.onNext(response);
+            responseObserver.onCompleted();
+            logger.log(Level.INFO, "Name of new user already exist");
+        }
+
+        if (usersDataBase.findUserRecordByName(newUser.getName()) != null) {
+            ServerError error = ServerError.newBuilder().setMessage("User's email already exist").setType(ServerError.ErrorType.INVALID_REQUEST).build();
+            ActionResultResponse response = ActionResultResponse.newBuilder().setError(error).build();
+            responseObserver.onNext(response);
+            logger.log(Level.INFO, "Email of new user already exist");
+            responseObserver.onCompleted();
+        }
+
+
+        //newUser.eMail = request.getEmail();
+        //newUser.userName = request.getUsername();
+        //newUser.password = request.getPassword();
+
+        /*for (var user: users) {
             if (user.userName.equals(newUser.userName)) {
                 ServerError error = ServerError.newBuilder().setMessage("User already exist").setType(ServerError.ErrorType.INVALID_REQUEST).build();
                 ActionResultResponse response = ActionResultResponse.newBuilder().setError(error).build();
@@ -50,11 +71,12 @@ public class AuthService extends AuthServiceGrpc.AuthServiceImplBase {
                 responseObserver.onCompleted();
                 return;
             }
-        }
+        }*/
 
         responseObserver.onNext(ActionResultResponse.newBuilder().build());
-        users.add(newUser);
-        logger.log(Level.INFO, "New user(Email: " + newUser.eMail + ", Name: " + newUser.userName + ", Password:" +newUser.password + ")");
+        usersDataBase.saveUserRecord(newUser);
+        //users.add(newUser);
+        logger.log(Level.INFO, "New user(Email: " + newUser.getEmail() + ", Name: " + newUser.getName() + ", Password:" +newUser.getHashedPassword() + ")");
 
         responseObserver.onCompleted();
 
@@ -65,11 +87,21 @@ public class AuthService extends AuthServiceGrpc.AuthServiceImplBase {
     public void login(LoginRequest request, StreamObserver<LoginResponse> responseObserver) {
         User newUser = new User();
 
-        newUser.eMail = request.getEmail();
-        newUser.password = request.getPassword();
-        logger.log(Level.INFO, "Next user(Email: " + newUser.eMail  + ", Password:" +newUser.password + ")");
+        //newUser.eMail = request.getEmail();
+        //newUser.password = request.getPassword();
+        newUser.setEmail(request.getEmail());
+        newUser.setHashedPassword(BCrypt.hashpw(request.getPassword(), BCrypt.gensalt()));
+        logger.log(Level.INFO, "Next user(Email: " + newUser.getEmail()  + ", Password:" +newUser.getHashedPassword() + ")");
 
-        for (var user: users) {
+        if (usersDataBase.findUserRecordByEmail(newUser.getEmail()) != null) {
+            LoginResponse response = LoginResponse.newBuilder().build();
+            responseObserver.onNext(response);
+            responseObserver.onCompleted();
+            logger.log(Level.INFO, "User is find");
+            return;
+        }
+
+        /*for (var user: users) {
             if (user.eMail.equals(newUser.eMail) && user.password.equals(newUser.password)) {
                 LoginResponse response = LoginResponse.newBuilder().build();
                 responseObserver.onNext(response);
@@ -77,7 +109,7 @@ public class AuthService extends AuthServiceGrpc.AuthServiceImplBase {
                 logger.log(Level.INFO, "User is find");
                 return;
             }
-        }
+        }*/
 
         logger.log(Level.INFO, "User not find");
         ServerError error = ServerError.newBuilder().setMessage("User not exist").setType(ServerError.ErrorType.INVALID_REQUEST).build();
