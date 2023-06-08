1dadd5a766b6e06fff1cdc6ed45d99d6d54c925b
dezd4r <naumkinvs03@gmail.com>
2023-06-08 16:45:20 +0200

    Adding a UsersDB class

---
+++ server/app/src/main/java/components/db/HibernateUtil.java
@@ -0,0 +1,21 @@
+package components.db;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+
+public class HibernateUtil {
+    private static final SessionFactory sessionFactory;
+
+    static {
+        try {
+            sessionFactory = new Configuration().configure("hibernate.cfg.xml").buildSessionFactory();
+        } catch (Throwable ex) {
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+}
--- server/app/src/main/java/components/db/UsersDataBase.java
+++ server/app/src/main/java/components/db/UsersDataBase.java
@@ -1,6 +1,7 @@
 package components.db;
 
 import org.hibernate.Session;
+import org.hibernate.Transaction;
 import org.hibernate.query.*;
 
 import java.util.List;
@@ -12,17 +13,54 @@ import java.util.logging.Logger;
 public class UsersDataBase {
     private final Logger logger = Logger.getLogger(UsersDataBase.class.getName());
 
-    public void saveUserRecord (Session session, User user) {
-        session.beginTransaction();
+    public void saveUserRecord (User user) {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Transaction transaction = session.beginTransaction();
         session.save(user);
-        session.getTransaction().commit();
+        transaction.commit();
+        session.close();
         logger.log(Level.INFO, "Next User is saved");
     }
 
-    public List<User> fetchUserRecordByEmail (Session session, String email) {
-        Query query = session.createQuery("SELECT * FROM users WHERE email = " + email);
-        List users = query.list();
-        return users;
+    public void updateUserRecord (User user) {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Transaction transaction = session.beginTransaction();
+        User newUser = session.get(User.class, user.getId());//?
+        newUser = user;
+        session.merge(newUser);
+        transaction.commit();
+        session.close();
+        logger.log(Level.INFO, "User is updated");
+    }
+
+    public User findUserRecordByEmail (String email) {
+        logger.log(Level.INFO, "Find Users in DataBase by email");
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Transaction transaction = session.beginTransaction();
+        Query<User> query = session.createQuery("FROM User WHERE email = :email", User.class);
+        query.setParameter("email", email);
+
+        User user = query.uniqueResult();
+
+        transaction.commit();
+        session.close();
+
+        return user;
+    }
+
+    public User findUserRecordByName (String name) {
+        logger.log(Level.INFO, "Find Users in DataBase by name");
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Transaction transaction = session.beginTransaction();
+        Query<User> query = session.createQuery("FROM User WHERE name = :name", User.class);
+        query.setParameter("name", name);
+
+        User user = query.uniqueResult();
+
+        transaction.commit();
+        session.close();
+
+        return user;
     }
 
 }
