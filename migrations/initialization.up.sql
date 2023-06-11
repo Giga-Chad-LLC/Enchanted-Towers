42395d933fb5397a20ee845b65b3a0ad1ad55c32
Vladislav Art <vladislav0art@gmail.com>
2023-06-11 22:02:39 +0200

    Create tokens table

--- migrations/initialization.up.sql
+++ migrations/initialization.up.sql
@@ -4,3 +4,10 @@ CREATE TABLE IF NOT EXISTS users (
     username VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL
 );
+
+
+CREATE TABLE IF NOT EXISTS tokens (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER UNIQUE REFERENCES users(id),
+    token VARCHAR(255) NOT NULL
+);
