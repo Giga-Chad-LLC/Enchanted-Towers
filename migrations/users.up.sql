ed7c3dd46dad16db668d3ffd090b83305cfd4a24
Vladislav Art <vladislav0art@gmail.com>
2023-06-11 15:23:07 +0200

    Add users table migrations sql files

---
+++ migrations/users.up.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    hashed_password VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL
+);
